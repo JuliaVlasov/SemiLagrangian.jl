5236cc6bdd1af8913da91c6daca67cc53fdb4fbb
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-03-11 22:10:33 +0100

    Update advection.jl

--- src/advection.jl
+++ src/advection.jl
@@ -5,8 +5,11 @@ export Advection
 """
     Advection(interpolation_type, mesh, LBC, RBC)
 
-Backward semi-lagrangian advection using spline interpolation.
-Domain is periodic and `p` is the spline degree
+Creates a 1d backward semi-lagrangian advection.
+
+- `interp`   : Interpolation type (Bspline(degree), Lagrange(degree))
+- `mesh`     : UniformMesh along advection direction
+- `LBC, RBC` : Boundary conditions type (:periodic, :Hermite)
 
 """
 mutable struct Advection 
@@ -23,6 +26,7 @@ mutable struct Advection
 
         if ( (LBC,RBC) == (:periodic, :periodic))
 
+            @assert isodd(interp.p)
             adv = BsplinePeriodic(interp, mesh)
 
         elseif ( (LBC, RBC) == (:Hermite, :Hermite))
@@ -31,10 +35,9 @@ mutable struct Advection
 
         else
 
-            throw( " This advection is not yet implemented ")
+            throw( " This kind of advection is not yet implemented ")
 
         end 
-
 
         new( interp, adv, dims )
 
@@ -44,15 +47,30 @@ mutable struct Advection
 end
 
 """
-    advection! = Advection(interpolation_type, mesh, LBC, RBC)
+    advection!(f, v, dt)
 
 Advection of a 2d function `f` discretized on a 2d `mesh`
-along the input axis at velocity `v`
+along the input axis at velocity `v`. This function is
+created from `Advector` callable type.
+
+```julia
+mesh = UniformMesh( -π, π, 64 )
+advection! = Advection( Bspline(3), mesh, 1, :periodic, :periodic )
+
+f = exp.( - mesh.points .^ 2 )
+
+dt = 0.5
+v  = ones( Float64, mesh.length)
+
+advection!( f, v, dt )
+
+
+
 
 """
-function (self :: Advection)(f    :: Array{Float64,2}, 
-                             v    :: Vector{Float64}, 
-                             dt   :: Float64)
+function (self :: Advection)(f  :: Array{Float64,2}, 
+                             v  :: Vector{Float64}, 
+                             dt :: Float64)
 
     p    = self.interp.p
     dims = self.dims

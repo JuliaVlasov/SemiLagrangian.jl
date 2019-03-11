487a5090e28587080e71374e55bb954ffe24b904
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-11 19:14:56 +0000

    build based on c970597

--- dev/search_index.js
+++ dev/search_index.js
@@ -29,55 +29,15 @@ var documenterSearchIndex = {"docs": [
     "page": "Functions",
     "title": "SemiLagrangian.Advection",
     "category": "type",
-    "text": "PeriodicAdvection(p, mesh)\n\nBackward semi-lagrangian advection using spline interpolation. Domain is periodic and p is the spline degree\n\n\n\n\n\n"
+    "text": "Advection(interpolation_type, mesh, LBC, RBC)\n\nBackward semi-lagrangian advection using spline interpolation. Domain is periodic and p is the spline degree\n\n\n\n\n\n"
 },
 
 {
-    "location": "functions/#SemiLagrangian.interpolate-Tuple{Array{Float64,1},Int64,Float64,Float64,Float64}",
+    "location": "functions/#SemiLagrangian.Advection-Tuple{Array{Float64,2},Array{Float64,1},Float64}",
     "page": "Functions",
-    "title": "SemiLagrangian.interpolate",
+    "title": "SemiLagrangian.Advection",
     "category": "method",
-    "text": "interpolate( coeffs, n, f)\n\nCompute interpolatted value at x from coefficients get  from compute_interpolants.\n\nThis function is a julia version of a Fortran code written by\n\nEdwin Chacon Golcher (Institute of Physics of the Czech Academy of Sciences)\n\n\n\n\n\n"
-},
-
-{
-    "location": "functions/#SemiLagrangian.interpolate-Tuple{Int64,Array{Complex{Float64},1},Float64,Float64}",
-    "page": "Functions",
-    "title": "SemiLagrangian.interpolate",
-    "category": "method",
-    "text": "interpolate( p, f, delta, alpha)\n\nCompute the interpolating spline of degree p of odd degree of a 1D function f on a periodic uniform mesh, at all points x-alpha input f is the Fourier transform and complex you have to in2erse transform after return\n\n\n\n\n\n"
-},
-
-{
-    "location": "functions/#SemiLagrangian.interpolate-Tuple{Int64,Array{Float64,1},Float64,Float64}",
-    "page": "Functions",
-    "title": "SemiLagrangian.interpolate",
-    "category": "method",
-    "text": "interpolate( p, f, delta, alpha)\n\nCompute the interpolating spline of degree p of odd degree of a 1D function f on a periodic uniform mesh, at all points x-alpha. f type is Vector{Float64}.\n\nDerived from a Python program written by  Eric Sonnendrucker (Max-Planck-Institut fur Plasmaphysik - Garching)\n\n\n\n\n\n"
-},
-
-{
-    "location": "functions/#SemiLagrangian.advection!-Tuple{Array{Complex{Float64},2},VlasovBase.UniformMesh,Array{Float64,1},Int64,Float64,BSpline}",
-    "page": "Functions",
-    "title": "SemiLagrangian.advection!",
-    "category": "method",
-    "text": "advection!(f, mesh, v, n2, dt, interp)\n\nAdvection of a 2d function f along its first dimension with velocity v. Since the fft are computed inplace, the function  must be represented by a Array{Complex{Float64},2}.\n\n\n\n\n\n"
-},
-
-{
-    "location": "functions/#SemiLagrangian.advection!-Tuple{Array{Float64,2},VlasovBase.UniformMesh,Array{Float64,1},Float64,CubicSpline,Int64}",
-    "page": "Functions",
-    "title": "SemiLagrangian.advection!",
-    "category": "method",
-    "text": " advection!( f, mesh1, v,  dt)\n\nSemi-lagrangian advection function of 2D distribution function represented  by array f. The advection operates along axis (=1 is most efficient)  with speed v during dt.\n\nIt uses cubic splines interpolation.\n\n\n\n\n\n"
-},
-
-{
-    "location": "functions/#SemiLagrangian.advection!-Tuple{Array{Float64,2},VlasovBase.UniformMesh,VlasovBase.UniformMesh,Array{Float64,1},Float64,BSpline,Int64}",
-    "page": "Functions",
-    "title": "SemiLagrangian.advection!",
-    "category": "method",
-    "text": "advection!( mesh, f, v, dt, interp, axis)\n\nAdvection of a 2d function f discretized on a 2d mesh along the input axis at velocity v\n\n\n\n\n\n"
+    "text": "advection! = Advection(interpolation_type, mesh, LBC, RBC)\n\nAdvection of a 2d function f discretized on a 2d mesh along the input axis at velocity v\n\n\n\n\n\n"
 },
 
 {
@@ -85,15 +45,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Functions",
     "title": "SemiLagrangian.bspline",
     "category": "method",
-    "text": "bspline(p, j, x)\n\nReturn the value at x in [0,1[ of the B-spline with integer nodes of degree p with support starting at j. Implemented recursively using the de Boor\'s recursion formula\n\nDerived from a Python program written by  Eric Sonnendrucker (Max-Planck-Institut fur Plasmaphysik - Garching))\n\n\n\n\n\n"
-},
-
-{
-    "location": "functions/#SemiLagrangian.compute_interpolants-Tuple{Int64,Array{Float64,N} where N}",
-    "page": "Functions",
-    "title": "SemiLagrangian.compute_interpolants",
-    "category": "method",
-    "text": "compute_interpolants( n, f)\n\nCompute interpolation coefficients\n\nThis function is a julia version of a Fortran code written by Edwin Chacon Golcher (Institute of Physics of the Czech Academy of Sciences)\n\n\n\n\n\n"
+    "text": "bspline(p, j, x)\n\nReturn the value at x in [0,1[ of the B-spline with integer nodes of degree p with support starting at j. Implemented recursively using the de Boor\'s recursion formula\n\nOriginal python program written by  Eric Sonnendrucker (Max-Planck-Institut fur Plasmaphysik - Garching (Germany))\n\n\n\n\n\n"
 },
 
 {
@@ -129,11 +81,27 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "functions/#SemiLagrangian.interpolate!-Tuple{Array{Complex{Float64},1},SemiLagrangian.BsplinePeriodic,Float64}",
+    "page": "Functions",
+    "title": "SemiLagrangian.interpolate!",
+    "category": "method",
+    "text": "interpolate!( f, bspl, alpha)\n\nCompute the interpolating spline of degree p of odd degree of a 1D function f on a periodic uniform mesh, at all points x-alpha\n\nArray representing f is complex and modified inplace.\n\n\n\n\n\n"
+},
+
+{
     "location": "functions/#SemiLagrangian.interpolate!-Tuple{SemiLagrangian.SplineNN,Array{Float64,2},Float64,Float64}",
     "page": "Functions",
     "title": "SemiLagrangian.interpolate!",
     "category": "method",
     "text": "interpolation par spline periodique dans les deux directions. Les points d\'interpolation sont definis grace a depx et depy qui definissent le deplacement par rapport au maillage.\n\nf contient les valeurs de la fonction de distribution\ndepx et depy : deplacements par rapport au maillage   des points dans les quels on veut evaluer la spline.\n\n\n\n\n\n"
+},
+
+{
+    "location": "functions/#SemiLagrangian.interpolate-Tuple{Array{Float64,1},SemiLagrangian.BsplinePeriodic,Float64}",
+    "page": "Functions",
+    "title": "SemiLagrangian.interpolate",
+    "category": "method",
+    "text": "interpolate( f, bspl, alpha)\n\nCompute the interpolating spline of degree p of odd degree of a 1D function f on a periodic uniform mesh, at all points x-alpha. f type is Vector{Float64}.\n\nDerived from a Python program written by  Eric Sonnendrucker (Max-Planck-Institut fur Plasmaphysik - Garching)\n\n\n\n\n\n"
 },
 
 {

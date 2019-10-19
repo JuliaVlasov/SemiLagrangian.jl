7e423b738555677856a26b8210ffa9a65b7a8dc0
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-10-19 17:22:33 +0200

    refactor fortran sources

--- src/advection.jl
+++ src/advection.jl
@@ -88,3 +88,68 @@ function (self :: Advection)(f  :: Array{Float64,2},
     end
 
 end
+import VlasovBase: UniformMesh
+
+"""
+    PeriodicAdvection(p, mesh)
+
+Backward semi-lagrangian advection using spline interpolation.
+Domain is periodic and `p` is the spline degree
+
+"""
+mutable struct PeriodicAdvection <: AbstractAdvection
+    
+    p        :: Int64 
+    mesh     :: UniformMesh
+    modes    :: Vector{Float64}
+    eig_bspl :: Vector{Float64}
+    eigalpha :: Vector{Complex{Float64}}
+    
+    function PeriodicAdvection( p, mesh )
+        nx        = mesh.nx
+        modes     = zeros(Float64, nx)
+        modes    .= [2π * i / nx for i in 0:nx-1]
+        eig_bspl  = zeros(Float64, nx)
+        eig_bspl  = zeros(Float64, nx)
+        eig_bspl .= bspline(p, -div(p+1,2), 0.0)
+        for i in 1:div(p+1,2)-1
+            eig_bspl .+= bspline(p, i-(p+1)÷2, 0.0) * 2 .* cos.(i * modes)
+        end
+        eigalpha  = zeros(Complex{Float64}, nx)
+        new( p, mesh, modes, eig_bspl, eigalpha )
+    end
+    
+end
+
+function (adv :: PeriodicAdvection)(f    :: Array{Complex{Float64},2}, 
+                                    v    :: Vector{Float64}, 
+                                    dt   :: Float64)
+    
+   nx = adv.mesh.nx
+   nv = length(v)
+   dx = adv.mesh.dx
+    
+   fft!(f,1)
+    
+   @inbounds for j in 1:nv
+      alpha = dt * v[j] / dx
+      # compute eigenvalues of cubic splines evaluated at displaced points
+      ishift = floor(-alpha)
+      beta   = -ishift - alpha
+      fill!(adv.eigalpha,0.0im)
+      for i in -div(adv.p-1,2):div(adv.p+1,2)
+         adv.eigalpha .+= (bspline(adv.p, i-div(adv.p+1,2), beta) 
+                        .* exp.((ishift+i) * 1im .* adv.modes))
+      end
+          
+      # compute interpolating spline using fft and properties of circulant matrices
+      
+      @inbounds for i in eachindex(adv.eigalpha)
+         f[i,j] *= adv.eigalpha[i] / adv.eig_bspl[i]
+      end
+        
+   end
+        
+   ifft!(f,1)
+    
+end

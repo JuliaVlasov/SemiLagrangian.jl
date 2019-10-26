0815ef1f4af8aff9ccbb227b5932eb916825dc09
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-10-26 22:07:48 +0200

    Update advection.jl

--- src/advection.jl
+++ src/advection.jl
@@ -12,30 +12,13 @@ Creates a 1d backward semi-lagrangian advection.
 """
 mutable struct Advection 
     
+    mesh   :: UniformMesh 
     interp :: InterpolationType 
     adv    :: AbstractAdvection
-    dims   :: Int
     
-    function Advection( interp :: InterpolationType, 
-                        mesh   :: UniformMesh,
-                        dims   :: Int,
-                        LBC    :: Symbol,
-                        RBC    :: Symbol)
-
-        if ( (LBC,RBC) == (:periodic, :periodic))
-
-            @assert isodd(interp.p)
-            adv = BsplinePeriodic(interp, mesh)
-
-        elseif ( (LBC, RBC) == (:Hermite, :Hermite))
-
-            adv = BsplineHermite(interp, mesh)
-
-        else
-
-            throw( " This kind of advection is not yet implemented ")
-
-        end 
+    function Advection( mesh   :: UniformMesh,
+                        interp :: InterpolationType, 
+                        bc     :: Symbol )
 
         new( interp, adv, dims )
 
@@ -53,7 +36,7 @@ created from `Advector` callable type.
 
 ```julia
 mesh = UniformMesh( -π, π, 64 )
-advection! = Advection( Bspline(3), mesh, 1, :periodic, :periodic )
+advection! = Advection( mesh, Bspline(3), :periodic )
 
 f = exp.( - mesh.points .^ 2 )
 
@@ -61,9 +44,6 @@ dt = 0.5
 v  = ones( Float64, mesh.length)
 
 advection!( f, v, dt )
-
-
-
 
 """
 function (self :: Advection)(f  :: Array{Float64,2}, 
@@ -87,66 +67,3 @@ function (self :: Advection)(f  :: Array{Float64,2},
 
 end
 
-"""
-    PeriodicAdvection(p, mesh)
-
-Backward semi-lagrangian advection using spline interpolation.
-Domain is periodic and `p` is the spline degree
-
-"""
-mutable struct PeriodicAdvection <: AbstractAdvection
-    
-    p        :: Int64 
-    mesh     :: UniformMesh
-    modes    :: Vector{Float64}
-    eig_bspl :: Vector{Float64}
-    eigalpha :: Vector{Complex{Float64}}
-    
-    function PeriodicAdvection( p, mesh )
-        nx        = mesh.nx
-        modes     = zeros(Float64, nx)
-        modes    .= [2π * i / nx for i in 0:nx-1]
-        eig_bspl  = zeros(Float64, nx)
-        eig_bspl  = zeros(Float64, nx)
-        eig_bspl .= bspline(p, -div(p+1,2), 0.0)
-        for i in 1:div(p+1,2)-1
-            eig_bspl .+= bspline(p, i-(p+1)÷2, 0.0) * 2 .* cos.(i * modes)
-        end
-        eigalpha  = zeros(Complex{Float64}, nx)
-        new( p, mesh, modes, eig_bspl, eigalpha )
-    end
-    
-end
-
-function (adv :: PeriodicAdvection)(f    :: Array{Complex{Float64},2}, 
-                                    v    :: Vector{Float64}, 
-                                    dt   :: Float64)
-    
-   nx = adv.mesh.nx
-   nv = length(v)
-   dx = adv.mesh.dx
-    
-   fft!(f,1)
-    
-   @inbounds for j in 1:nv
-      alpha = dt * v[j] / dx
-      # compute eigenvalues of cubic splines evaluated at displaced points
-      ishift = floor(-alpha)
-      beta   = -ishift - alpha
-      fill!(adv.eigalpha,0.0im)
-      for i in -div(adv.p-1,2):div(adv.p+1,2)
-         adv.eigalpha .+= (bspline(adv.p, i-div(adv.p+1,2), beta) 
-                        .* exp.((ishift+i) * 1im .* adv.modes))
-      end
-          
-      # compute interpolating spline using fft and properties of circulant matrices
-      
-      @inbounds for i in eachindex(adv.eigalpha)
-         f[i,j] *= adv.eigalpha[i] / adv.eig_bspl[i]
-      end
-        
-   end
-        
-   ifft!(f,1)
-    
-end

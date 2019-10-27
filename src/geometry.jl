d05bd8daeb71568a6874e73588df7b0b5f9777d5
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-10-27 13:28:36 +0100

    Update geometry.jl

--- src/geometry.jl
+++ src/geometry.jl
@@ -2,60 +2,59 @@ export Geometry
 
 struct Geometry
 
-    n1 :: Int64
-    n2 :: Int64
+    n1::Int64
+    n2::Int64
 
-    x1min  :: Float64
-    x2min  :: Float64
+    x1min::Float64
+    x2min::Float64
 
-    delta1 :: Float64
-    delta2 :: Float64
-    
-    x1grid :: Array{Float64,1}
-    x2grid :: Array{Float64,1}
+    delta1::Float64
+    delta2::Float64
 
-    bc :: Symbol
+    x1grid::Array{Float64,1}
+    x2grid::Array{Float64,1}
 
-"""
-initialize geometry from origin cordinates, 
-space steps and number of points
-"""
+    bc::Symbol
+
+    """
+    initialize geometry from origin cordinates,
+    space steps and number of points
+    """
     function Geometry(n1, n2, x1min, x2min, delta1, delta2)
 
-        x1grid = collect(range(x1min, length=n1, step=delta1))
-        x2grid = collect(range(x2min, length=n2, step=delta2))
+        x1grid = collect(range(x1min, length = n1, step = delta1))
+        x2grid = collect(range(x2min, length = n2, step = delta2))
 
-        new(n1, n2, x1min, x2min, delta1, delta2, x1grid, x2grid, :none) 
+        new(n1, n2, x1min, x2min, delta1, delta2, x1grid, x2grid, :none)
 
-    end 
+    end
 
-"""
-initialize geometry from origin and end coordinates and 
-number of points
-"""
+    """
+    initialize geometry from origin and end coordinates and
+    number of points
+    """
     function Geometry(n1, n2, x1, x2, bc::Symbol)
 
-	x1min, x1max = x1
-	x2min, x2max = x2
-   
-        if ( (bc == :perxy) || (bc == :perx) ) 
-           x1grid = collect(range(x1min, stop=x1max, length=n1+1)[1:end-1])
+        x1min, x1max = x1
+        x2min, x2max = x2
+
+        if ((bc == :perxy) || (bc == :perx))
+            x1grid = collect(range(x1min, stop = x1max, length = n1 + 1)[1:end-1])
         else
-           x1grid = collect(range(x1min, stop=x1max, length=n1))
+            x1grid = collect(range(x1min, stop = x1max, length = n1))
         end
 
-        if ( (bc == :perxy) || (bc == :pery) ) 
-           x2grid = collect(range(x2min, stop=x2max, length=n2+1)[1:end-1])
+        if ((bc == :perxy) || (bc == :pery))
+            x2grid = collect(range(x2min, stop = x2max, length = n2 + 1)[1:end-1])
         else
-           x2grid = collect(range(x2min, stop=x2max, length=n2))
+            x2grid = collect(range(x2min, stop = x2max, length = n2))
         end
 
         delta1 = x1grid[2] - x1grid[1]
         delta2 = x2grid[2] - x2grid[1]
 
-        new(n1, n2, x1min, x2min, delta1, delta2, x1grid, x2grid, bc) 
+        new(n1, n2, x1min, x2min, delta1, delta2, x1grid, x2grid, bc)
 
-    end 
+    end
 
 end
-

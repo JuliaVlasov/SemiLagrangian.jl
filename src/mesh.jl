2b033e6b1443dc33d7afc8ef56c87bcebd029b19
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-10-27 13:28:45 +0100

    Update mesh.jl

--- src/mesh.jl
+++ src/mesh.jl
@@ -20,28 +20,28 @@ If you want remove the last point for periodic domain, set endpoint=false
 """
 struct UniformMesh
 
-   start    :: Float64
-   stop     :: Float64
-   length   :: Int
-   step     :: Float64
-   points   :: Vector{Float64}
-   width    :: Float64
+    start::Float64
+    stop::Float64
+    length::Int
+    step::Float64
+    points::Vector{Float64}
+    width::Float64
 
-   function UniformMesh(start, stop, length::Int; endpoint=true)
+    function UniformMesh(start, stop, length::Int; endpoint = true)
 
-       if (endpoint)
-           points = range(start, stop=stop, length=length)
-       else
-           points = range(start, stop=stop, length=length+1)[1:end-1]
-       end
+        if (endpoint)
+            points = range(start, stop = stop, length = length)
+        else
+            points = range(start, stop = stop, length = length + 1)[1:end-1]
+        end
 
-       step = points.step
+        step = points.step
 
-       width = stop - start
+        width = stop - start
 
-       new( start, stop, length, step, points, width)
+        new(start, stop, length, step, points, width)
 
-   end
+    end
 
 end
 
@@ -49,22 +49,24 @@ export compute_charge!
 
 """
     compute_rho!( rho, mesh_v, fvx)
- 
+
  Compute charge density
 
  ρ(x,t) = ∫ f(x,v,t) dv
 
 """
-function compute_charge!(rho   :: Vector{Float64}, 
-                         meshv :: UniformMesh,
-                         fvx   :: Array{Float64,2})
+function compute_charge!(
+    rho::Vector{Float64},
+    meshv::UniformMesh,
+    fvx::Array{Float64,2},
+)
 
-    dv   =  meshv.step
-    rho .=  dv .* vec(sum(fvx, dims=1))
+    dv = meshv.step
+    rho .= dv .* vec(sum(fvx, dims = 1))
     rho .= rho .- mean(rho)
 
 end
- 
+
 
 export compute_e!
 
@@ -78,15 +80,17 @@ Inplace computation of electric field. Fields e and rho are
 already allocated.
 
 """
-function compute_e!(e     :: Vector{Float64},
-		    meshx :: UniformMesh,
-		    rho   :: Vector{Float64})
+function compute_e!(
+    e::Vector{Float64},
+    meshx::UniformMesh,
+    rho::Vector{Float64},
+)
 
-   nx = meshx.length
-   k = 2π / (meshx.stop - meshx.start)
-   modes  = zeros(Float64, nx)
-   modes .= k .* vcat(0:nx÷2-1,-nx÷2:-1)
-   modes[1] = 1.0
-   e .= real(ifft(-1im .* fft(rho) ./ modes))
+    nx = meshx.length
+    k = 2π / (meshx.stop - meshx.start)
+    modes = zeros(Float64, nx)
+    modes .= k .* vcat(0:nx÷2-1, -nx÷2:-1)
+    modes[1] = 1.0
+    e .= real(ifft(-1im .* fft(rho) ./ modes))
 
 end

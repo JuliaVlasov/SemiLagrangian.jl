8d6fdaa1411d82e4832f96e47e79ed32c66cacc1
Yves Mocquard <yves.mocquard@irisa.fr>
2021-02-04 08:52:39 +0100

    Update make.jl
    Correct spelling of my name

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ ENV["GKSwstype"] = "100"
 
 makedocs(modules=[SemiLagrangian],
          sitename = "SemiLagrangian.jl",
-         authors = "Yves Mocquart, Pierre Navaro and Nicolas Crouseilles",
+         authors = "Yves Mocquard, Pierre Navaro and Nicolas Crouseilles",
          format=Documenter.HTML(;
          prettyurls=get(ENV, "CI", "false") == "true",
          canonical="https://juliavlasov.github.io/SemiLagrangian.jl",

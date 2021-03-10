be86de1fa506daf5d9a70682c8c40de14040bdf8
Yves Mocquard <yves.mocquard@irisa.fr>
2021-03-10 11:21:47 +0100

    last refining rotation 2d

--- test/test_rotation.jl
+++ test/test_rotation.jl
@@ -18,7 +18,7 @@ function exact!(f, mesh1::UniformMesh{T}, mesh2::UniformMesh{T}, tf::T) where {T
     for (i, x) in enumerate(mesh1.points), (j, y) in enumerate(mesh2.points)
         s, c = sincos(tf)
         xn, yn = c * x - s * y, + s * x + c * y
-        f[i,j] = exp(-4*((xn)^2+(yn+T(6//5))^2))
+        f[i,j] = exp(-5*((xn)^2+(yn+T(6//5))^2))
     end
 end
 
@@ -64,8 +64,8 @@ function test_rotation(
     interp::AbstractInterpolation2d{T}, 
     nbdt::Int
 ) where{T}
-    spmin, spmax, nsp =  T(-5), T(5),  sz[1]
-    vmin, vmax, nv = -T(5), T(5), sz[2]
+    spmin, spmax, nsp =  T(-4.5), T(5),  sz[1]
+    vmin, vmax, nv = -T(5.2), T(5), sz[2]
 
     mesh_sp = UniformMesh( spmin, spmax, nsp)
     mesh_v = UniformMesh( vmin, vmax, nv)
@@ -76,17 +76,17 @@ function test_rotation(
     # dec2 = 2tan(dt/2) / step(mesh_v) * mesh_sp.points
     dec1 = zeros(T, sz)
     dec2 = zeros(T, sz)
-    coef = 1/(1+dt^2/4)
-    cor = 2sin(dt/2)
+    tgdt = 2tan(dt/2)
+    coef = 1/(1+tgdt^2/4)
     for i=1:sz[1], j=1:sz[2]
-        x = mesh_sp.points[i] /step(mesh_v)
-        y = mesh_v.points[j]/ step(mesh_sp)
-        v = [- dt * coef * (y + dt*x/2) , (dt * coef * (x - dt*y/2))]
-        nv = norm(v)
-        nr = norm([x,y])
-        if nv != 0
-            v *= cor*nr/nv
-        end
+        x = mesh_sp.points[i] 
+        y = mesh_v.points[j]
+        v = [- tgdt * coef * (y + tgdt*x/2)/ step(mesh_sp) , (tgdt * coef * (x - tgdt*y/2))/step(mesh_v)]
+        # nv = norm(v)
+        # nr = norm([x,y])
+        # if nv != 0
+        #     v *= cor*nr/nv
+        # end
         dec1[i,j], dec2[i,j] = v
     end
 
@@ -118,15 +118,17 @@ end
 
 @testset "test rotation" begin
     T = Float64
-    @time @test test_rotation((200, 200), Lagrange2d(5, T), 100) < 1e-3
-    @time @test test_rotation((200, 220), Lagrange(5, T), Lagrange(5, T), 11) < 1e-3
+    @time @test test_rotation((100, 122), Lagrange2d(5, T), 11) < 1e-3
+    @time @test test_rotation((200, 222), Lagrange(5, T), Lagrange(5, T), 11) < 1e-3
     @time @test test_rotation((128, 256), B_SplineLU(5, 128, T), B_SplineLU(5, 256, T), 11) < 1e-3
     @time @test test_rotation((128, 256), B_SplineFFT(5, 128, T), B_SplineFFT(5, 256, T), 11) < 1e-3
     T = Double64
+    @time @test test_rotation((100, 122), Lagrange2d(15, T), 11) < 1e-6
     @time @test test_rotation((200, 220), Lagrange(15, T), Lagrange(15, T), 11) < 1e-7
     @time @test test_rotation((128, 256), B_SplineLU(15, 128, T), B_SplineLU(15, 256, T), 11) < 1e-8
     @time @test test_rotation((128, 256), B_SplineFFT(15, 128, T), B_SplineFFT(15, 256, T), 11) < 1e-8
     T = BigFloat
+    @time @test test_rotation((100, 122), Lagrange2d(21, T), 11) < 1e-7
     @time @test test_rotation((200, 220), Lagrange(25, T), Lagrange(25, T), 11) < 1e-10
     @time @test test_rotation((128, 256), B_SplineLU(25, 128, T), B_SplineLU(25, 256, T), 11) < 1e-11
     @time @test test_rotation((128, 256), B_SplineFFT(25, 128, T), B_SplineFFT(25, 256, T), 11) < 1e-11

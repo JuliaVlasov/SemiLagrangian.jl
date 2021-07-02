c1ca0a88a3fca507aaa2c7ccc1773950b2c3a92b
Yves Mocquard <yves.mocquard@irisa.fr>
2021-07-02 18:07:29 +0200

    delete of mpoly

--- test/mpoly/mpoly.jl
+++
@@ -1,5 +0,0 @@
-x, y = generators(MPoly{Float64}, :x, :y)
-
-@test vars(x) == [:x, :y]
-@test nvars(x) == 2
-@test eltype(x) == Float64
--- test/mpoly/mpolyprinting.jl
+++
@@ -1,4 +0,0 @@
-x, y = generators(MPoly{Float64}, :x, :y)
-
-@test string((x+y)^3) == "x^3 + 3.0x^2*y + 3.0x*y^2 + y^3"
-@test string((x*y)^(-2)) == "x^(-2)*y^(-2)"
